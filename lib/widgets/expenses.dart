625d2448188a3aa3559db554d5a2e80cfd8486cd
Yuri <yuri.oda.lily@gmail.com>
2023-10-14 22:33:41 +0900

    add amount and cancel button

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import "package:expense_tracker/models/expense.dart";
 
@@ -25,15 +26,32 @@ class _ExpensesState extends State<Expenses> {
         date: DateTime.now(),
         category: Category.leisure)
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return const NewExpense();
+        });
+  }
+
   // ショートカット：build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: _openAddExpenseOverlay,
+              icon: const Icon(Icons.add),
+            )
+          ],
+        ),
         body: Column(
-      children: [
-        const Text("The chart"),
-        Expanded(child: ExpensesList(expenses: _registeredExpenses)),
-      ],
-    ));
+          children: [
+            const Text("The chart"),
+            Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          ],
+        ));
   }
 }
